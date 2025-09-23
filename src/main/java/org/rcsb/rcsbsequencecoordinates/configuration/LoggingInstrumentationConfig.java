a0895ee6d466e187d2b8993e9311d6d8170aa8c8
bioinsilico <jsegurmor@gmail.com>
2025-09-23 11:17:27 -0700

    log graphql queries at info level && properties fixed

---
+++ src/main/java/org/rcsb/rcsbsequencecoordinates/configuration/LoggingInstrumentationConfig.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2025 - now, RCSB PDB and contributors, licensed under MIT, See LICENSE file for more info.
+ */
+
+package org.rcsb.rcsbsequencecoordinates.configuration;
+
+import graphql.ExecutionInput;
+import graphql.execution.instrumentation.Instrumentation;
+import graphql.execution.instrumentation.InstrumentationState;
+import graphql.execution.instrumentation.SimplePerformantInstrumentation;
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.jspecify.annotations.NonNull;
+
+/**
+ * @author : joan
+ * @mailto : joan.segura@rcsb.org
+ * @created : 9/23/25, Tuesday
+ **/
+
+@Configuration
+public class LoggingInstrumentationConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingInstrumentationConfig.class);
+
+    @Bean
+    public Instrumentation loggingInstrumentation() {
+        return new SimplePerformantInstrumentation() {
+            @Override
+            public @NonNull ExecutionInput instrumentExecutionInput(
+                    ExecutionInput executionInput,
+                    InstrumentationExecutionParameters parameters,
+                    InstrumentationState state
+            ) {
+                log.info("GraphQL query:\n{}\nvariables: {}", executionInput.getQuery(), executionInput.getVariables());
+                return executionInput;
+            }
+        };
+    }
+
+}
